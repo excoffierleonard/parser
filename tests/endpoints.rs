22aa2c8d092704ca7b7017a0b5ec48d1d1ff1b3b
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-27 17:51:41 -0500

    tests: now all the test passes.

--- tests/endpoints.rs
+++ tests/endpoints.rs
@@ -1,4 +1,7 @@
-use actix_web::{test, App};
+use actix_web::{
+    http::header::{HeaderName, HeaderValue},
+    test, App,
+};
 use parser::routes::{greet, parse_file};
 use serde::Deserialize;
 
@@ -10,6 +13,36 @@ struct HelloResponse {
 #[derive(Deserialize)]
 struct ParseResponse {
     text: String,
+}
+
+// Since Actix Test does not support native multipart payload, we have to build our own.
+pub fn build_multipart_payload(
+    file_name: &str,
+    file_contents: &[u8],
+) -> (Vec<u8>, (HeaderName, HeaderValue)) {
+    let boundary = "-----------------------------202022185716362916172375148227";
+
+    let mut payload = Vec::new();
+
+    // Add form field boundary and headers
+    payload.extend_from_slice(format!("--{boundary}\r\n").as_bytes());
+    payload.extend_from_slice(
+        format!("Content-Disposition: form-data; name=\"file\"; filename=\"{file_name}\"\r\n\r\n")
+            .as_bytes(),
+    );
+
+    // Add file contents as raw bytes
+    payload.extend_from_slice(file_contents);
+
+    // Add closing boundary
+    payload.extend_from_slice(format!("\r\n--{boundary}--\r\n").as_bytes());
+
+    let header = (
+        actix_web::http::header::CONTENT_TYPE,
+        HeaderValue::from_str(&format!("multipart/form-data; boundary={boundary}")).unwrap(),
+    );
+
+    (payload, header)
 }
 
 // Tests the default route
@@ -39,19 +72,20 @@ async fn post_parse_pdf_1() {
     // Setup
     let app = test::init_service(App::new().service(parse_file)).await;
 
-    // NOTE: Maybe create a temporary pdf rather than having stored one in inputs
     // Read file
     let file_bytes = std::fs::read("tests/inputs/test_pdf_1.pdf").unwrap();
+    let (payload, content_type_header) = build_multipart_payload("test_pdf_1.pdf", &file_bytes);
 
     // Create request
     let req = test::TestRequest::post()
         .uri("/parse")
-        .set_payload(file_bytes)
-        .insert_header(("content-type", "multipart/form-data"))
+        .set_payload(payload)
+        .insert_header(content_type_header)
         .to_request();
 
     // Get response
     let resp = test::call_service(&app, req).await;
+    println!("{:?}", resp);
 
     // Assert the results
     let status = resp.status();
@@ -66,24 +100,28 @@ async fn post_parse_pdf_2() {
     // Setup
     let app = test::init_service(App::new().service(parse_file)).await;
 
-    // NOTE: Maybe create a temporary pdf rather than having stored one in inputs
     // Read file
     let file_bytes = std::fs::read("tests/inputs/test_pdf_2.pdf").unwrap();
+    let (payload, content_type_header) = build_multipart_payload("test_pdf_1.pdf", &file_bytes);
 
     // Create request
     let req = test::TestRequest::post()
         .uri("/parse")
-        .set_payload(file_bytes)
-        .insert_header(("content-type", "multipart/form-data"))
+        .set_payload(payload)
+        .insert_header(content_type_header)
         .to_request();
 
     // Get response
     let resp = test::call_service(&app, req).await;
+    println!("{:?}", resp);
 
     // Assert the results
     let status = resp.status();
     assert!(status.is_success());
 
     let body: ParseResponse = test::read_body_json(resp).await;
-    assert_eq!(body.text, "Hello, this is a test pdf for the parsing API.");
+    assert_eq!(
+        body.text,
+        "Hello, this is another test pdf for the parsing API."
+    );
 }
