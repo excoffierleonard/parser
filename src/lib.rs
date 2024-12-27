3da1679b79aa0b39edcd84c3799ef400fc29266a
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-27 00:54:07 -0500

    test: created the final integration test structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-use actix_web::{get, web, Responder};
+use actix_web::{get, post, web, Responder};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
+}
+
+#[post("/parse")]
+async fn parse() -> impl Responder {
+    format!("Default")
 }
--- tests/endpoints.rs
+++ tests/endpoints.rs
@@ -1,13 +1,48 @@
 use actix_web::{test, App};
-use parser::greet;
+use parser::{greet, parse};
 
+// Tests the default route
 #[actix_web::test]
 async fn get_hello() {
+    // Setup
     let app = test::init_service(App::new().service(greet)).await;
+
+    // Create request
     let req = test::TestRequest::get()
         .uri("/hello/test_name")
         .to_request();
+
+    // Get response
     let resp = test::call_service(&app, req).await;
+
+    // Assert the results
     assert!(resp.status().is_success());
     assert_eq!(test::read_body(resp).await, "Hello test_name!");
+}
+
+// This is the final integration test, it supposed to fail a lot before passing to comfirm the final implementation of the parsing endpoint
+#[actix_web::test]
+async fn post_parse_pdf() {
+    // Setup
+    let app = test::init_service(App::new().service(parse)).await;
+
+    // Read file
+    let file_bytes = include_bytes!("../tests/inputs/test_pdf.pdf");
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/parse")
+        .set_payload(file_bytes.to_vec())
+        .insert_header(("content-type", "multipart/form-data"))
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+
+    // Assert the results
+    assert!(resp.status().is_success());
+    assert_eq!(
+        test::read_body(resp).await,
+        "Hello, this is a test pdf for the parsing API."
+    );
 }
