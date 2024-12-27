41c44cf2e5ccd221cdfb7482042068a2ff530a41
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-27 15:34:13 -0500

    test: added another integrationt est with an other pdf to make differentation works

--- tests/endpoints.rs
+++ tests/endpoints.rs
@@ -36,13 +36,13 @@ async fn get_hello() {
 
 // This is the final integration test, it supposed to fail a lot before passing to comfirm the final implementation of the parsing endpoint
 #[actix_web::test]
-async fn post_parse_pdf() {
+async fn post_parse_pdf_1() {
     // Setup
     let app = test::init_service(App::new().service(parse_file)).await;
 
     // NOTE: Maybe create a temporary pdf rather than having stored one in inputs
     // Read file
-    let file_bytes = std::fs::read("tests/inputs/test_pdf.pdf").unwrap();
+    let file_bytes = std::fs::read("tests/inputs/test_pdf_1.pdf").unwrap();
 
     // Create request
     let req = test::TestRequest::post()
@@ -59,4 +59,33 @@ async fn post_parse_pdf() {
     // Assert the results
     assert!(status.is_success());
     assert_eq!(body.text, "Hello, this is a test pdf for the parsing API.");
+}
+
+#[actix_web::test]
+async fn post_parse_pdf_2() {
+    // Setup
+    let app = test::init_service(App::new().service(parse_file)).await;
+
+    // NOTE: Maybe create a temporary pdf rather than having stored one in inputs
+    // Read file
+    let file_bytes = std::fs::read("tests/inputs/test_pdf_2.pdf").unwrap();
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/parse")
+        .set_payload(file_bytes)
+        .insert_header(("content-type", "multipart/form-data"))
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+    let status = resp.status();
+    let body: ParseResponse = test::read_body_json(resp).await;
+
+    // Assert the results
+    assert!(status.is_success());
+    assert_eq!(
+        body.text,
+        "Hello, this is another test pdf for the parsing API."
+    );
 }
