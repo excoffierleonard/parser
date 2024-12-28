7423e002aee9c7c4d7b30ebfd4b825c61ec4b1b4
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-28 01:24:41 -0500

    now more reliable way to parse for the mime type

--- src/routes/parse.rs
+++ src/routes/parse.rs
@@ -2,6 +2,7 @@ use crate::errors::ApiError;
 use actix_multipart::Multipart;
 use actix_web::{post, HttpResponse};
 use futures_util::TryStreamExt;
+use infer::get_from_path;
 use mime::{Mime, APPLICATION_PDF};
 use pdf_extract::extract_text;
 use serde::Serialize;
@@ -15,8 +16,9 @@ struct Response {
 
 #[post("/parse")]
 async fn parse_file(mut payload: Multipart) -> Result<HttpResponse, ApiError> {
-    let (temp_file, content_type) = create_temp_file(&mut payload).await?;
+    let temp_file = create_temp_file(&mut payload).await?;
     let temp_file_path = get_temp_file_path(&temp_file)?;
+    let content_type = determine_mime_type(temp_file_path);
 
     let parsed_text = match content_type.as_ref() {
         Some(mime) if *mime == APPLICATION_PDF => parse_pdf(temp_file_path)?,
@@ -32,20 +34,15 @@ async fn parse_file(mut payload: Multipart) -> Result<HttpResponse, ApiError> {
     Ok(HttpResponse::Ok().json(Response { text: parsed_text }))
 }
 
-async fn create_temp_file(
-    payload: &mut Multipart,
-) -> Result<(NamedTempFile, Option<Mime>), ApiError> {
+async fn create_temp_file(payload: &mut Multipart) -> Result<NamedTempFile, ApiError> {
     let mut temp_file = NamedTempFile::new()
         .map_err(|e| ApiError::InternalError(format!("Failed to create temp file: {}", e)))?;
-    let mut content_type = None;
 
     while let Some(mut field) = payload
         .try_next()
         .await
         .map_err(|e| ApiError::InternalError(format!("Failed to process multipart: {}", e)))?
     {
-        content_type = determine_mime_type(&field);
-
         while let Some(chunk) = field
             .try_next()
             .await
@@ -57,18 +54,7 @@ async fn create_temp_file(
         }
     }
 
-    Ok((temp_file, content_type))
-}
-
-fn determine_mime_type(field: &actix_multipart::Field) -> Option<Mime> {
-    if let Some(cd) = field.content_disposition() {
-        if let Some(filename) = cd.get_filename() {
-            if filename.ends_with(".pdf") {
-                return Some(APPLICATION_PDF);
-            }
-        }
-    }
-    None
+    Ok(temp_file)
 }
 
 fn get_temp_file_path(temp_file: &NamedTempFile) -> Result<&str, ApiError> {
@@ -76,6 +62,13 @@ fn get_temp_file_path(temp_file: &NamedTempFile) -> Result<&str, ApiError> {
         .path()
         .to_str()
         .ok_or_else(|| ApiError::InternalError("Invalid temporary file path".to_string()))
+}
+
+fn determine_mime_type(file_path: &str) -> Option<Mime> {
+    get_from_path(file_path)
+        .ok()
+        .flatten()
+        .and_then(|kind| kind.mime_type().parse().ok())
 }
 
 fn parse_pdf(file_path: &str) -> Result<String, ApiError> {
@@ -95,18 +88,21 @@ mod tests {
     }
 
     #[test]
-    fn determine_mime_success() {
-        // Need to implement a unit test for that if possible
-        assert!(1 == 1);
-    }
-
-    #[test]
     fn get_temp_file_path_success() {
         let temp_file = NamedTempFile::new().unwrap();
         let result = get_temp_file_path(&temp_file).unwrap();
 
         assert!(result.len() > 0);
         assert!(result.starts_with("/tmp"));
+    }
+
+    #[test]
+    fn determine_mime_success() {
+        let file_path = "tests/inputs/test_pdf_1.pdf";
+        let result = determine_mime_type(file_path);
+
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), APPLICATION_PDF);
     }
 
     #[test]
