13221c88d9cff7d47d53536e57c12ff2a0182c34
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-28 11:51:32 -0500

    test: added a second doc test

--- tests/endpoints.rs
+++ tests/endpoints.rs
@@ -101,7 +101,7 @@ async fn post_parse_pdf_2() {
 
     // Read file
     let file_bytes = std::fs::read("tests/inputs/test_pdf_2.pdf").unwrap();
-    let (payload, content_type_header) = build_multipart_payload("test_pdf_1.pdf", &file_bytes);
+    let (payload, content_type_header) = build_multipart_payload("test_pdf_2.pdf", &file_bytes);
 
     // Create request
     let req = test::TestRequest::post()
@@ -121,5 +121,62 @@ async fn post_parse_pdf_2() {
     assert_eq!(
         body.text,
         "Hello, this is another test pdf for the parsing API."
+    );
+}
+
+#[actix_web::test]
+async fn post_parse_docx_1() {
+    // Setup
+    let app = test::init_service(App::new().service(parse_file)).await;
+
+    // Read file
+    let file_bytes = std::fs::read("tests/inputs/test_docx_1.docx").unwrap();
+    let (payload, content_type_header) = build_multipart_payload("test_docx_1.docx", &file_bytes);
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/parse")
+        .set_payload(payload)
+        .insert_header(content_type_header)
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+
+    // Assert the results
+    let status = resp.status();
+    assert!(status.is_success());
+
+    let body: ParseResponse = test::read_body_json(resp).await;
+    assert_eq!(body.text, "Hello, this is a test docx for the parsing API.");
+}
+
+#[actix_web::test]
+async fn post_parse_docx_2() {
+    // Setup
+    let app = test::init_service(App::new().service(parse_file)).await;
+
+    // Read file
+    let file_bytes = std::fs::read("tests/inputs/test_docx_2.docx").unwrap();
+    let (payload, content_type_header) = build_multipart_payload("test_docx_2.docx", &file_bytes);
+
+    // Create request
+    let req = test::TestRequest::post()
+        .uri("/parse")
+        .set_payload(payload)
+        .insert_header(content_type_header)
+        .to_request();
+
+    // Get response
+    let resp = test::call_service(&app, req).await;
+
+    // Assert the results
+    let status = resp.status();
+    assert!(status.is_success());
+
+    let body: ParseResponse = test::read_body_json(resp).await;
+    assert_eq!(
+        body.text,
+        "Hello, this is another test docx for the parsing API."
     );
 }
