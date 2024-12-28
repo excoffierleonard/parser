73c82b2b09d5c43b181382c4b209a3164a9a2806
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-28 01:36:53 -0500

    added logging capabilities.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use actix_web::{App, HttpServer};
+use actix_web::{
+    middleware::{Compress, Logger},
+    App, HttpServer,
+};
+use env_logger::Env;
+use num_cpus;
 use parser::routes::{greet, parse_file};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet).service(parse_file))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Compress::default())
+            .wrap(Logger::default())
+            .service(greet)
+            .service(parse_file)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .workers(num_cpus::get())
+    .run()
+    .await
 }
