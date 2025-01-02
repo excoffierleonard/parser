5052b2f5f65b379c0b172b43b25a3c66b4c81efe
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-01-02 18:21:03 -0500

    moved all the parsing logic to the core lib

--- parser-core/src/parsers.rs
+++ parser-core/src/parsers.rs
@@ -17,3 +17,135 @@ pub use pdf::parse_pdf;
 pub use pptx::parse_pptx;
 pub use text::parse_text;
 pub use xlsx::parse_xlsx;
+
+use crate::errors::ParserError;
+use infer;
+use mime::{Mime, APPLICATION_PDF, IMAGE, TEXT, TEXT_PLAIN};
+use std::fs::read_to_string;
+
+// Types not defined in the mime package
+const APPLICATION_DOCX: &str =
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+const APPLICATION_XLSX: &str = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+const APPLICATION_PPTX: &str =
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+
+/// Automatically detects the file type and uses the appropriate parser
+pub fn parse_any(file_path: &str) -> Result<String, ParserError> {
+    match determine_mime_type(file_path) {
+        Some(mime) if mime == APPLICATION_PDF => parse_pdf(file_path),
+        Some(mime) if mime == APPLICATION_DOCX => parse_docx(file_path),
+        Some(mime) if mime == APPLICATION_XLSX => parse_xlsx(file_path),
+        Some(mime) if mime == APPLICATION_PPTX => parse_pptx(file_path),
+        Some(mime) if mime.type_() == TEXT => parse_text(file_path),
+        Some(mime) if mime.type_() == IMAGE => parse_image(file_path),
+        Some(mime) => Err(ParserError::InvalidFormat(format!(
+            "Unsupported mime type: {}",
+            mime
+        ))),
+        None => Err(ParserError::InvalidFormat(
+            "Could not determine file type".into(),
+        )),
+    }
+}
+
+fn determine_mime_type(file_path: &str) -> Option<Mime> {
+    // First try to detect using file signatures
+    if let Some(kind) = infer::get_from_path(file_path).ok().flatten() {
+        if let Ok(mime) = kind.mime_type().parse() {
+            return Some(mime);
+        }
+    }
+
+    // TODO: Add specific function for special text data that needs formatting like CSV etc..
+    // TODO: Maybe add checks for false positive images, like svg that may be coerced to text but shouldnt.
+
+    // If no specific type was detected, check if it's readable as text
+    read_to_string(file_path).ok().map(|_| TEXT_PLAIN)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_any_success() {
+        // Already tested in the specific parser tests
+        assert!(1 == 1);
+    }
+
+    #[test]
+    fn determine_mime_success() {
+        // Testing for pdf detection
+        let file_path_pdf = "tests/inputs/test_pdf_1.pdf";
+        let result_pdf = determine_mime_type(file_path_pdf);
+
+        assert!(result_pdf.is_some());
+        assert_eq!(result_pdf.unwrap(), APPLICATION_PDF);
+
+        // Testing for docx detection
+        let file_path_docx = "tests/inputs/test_docx_1.docx";
+        let result_docx = determine_mime_type(file_path_docx);
+
+        assert!(result_docx.is_some());
+        assert_eq!(result_docx.unwrap(), APPLICATION_DOCX);
+
+        // Testing for xlsx detection
+        let file_path_xlsx = "tests/inputs/test_xlsx_1.xlsx";
+        let result_xlsx = determine_mime_type(file_path_xlsx);
+
+        assert!(result_xlsx.is_some());
+        assert_eq!(result_xlsx.unwrap(), APPLICATION_XLSX);
+
+        // Testing for pptx detection
+        let file_path_pptx = "tests/inputs/test_pptx_1.pptx";
+        let result_pptx = determine_mime_type(file_path_pptx);
+
+        assert!(result_pptx.is_some());
+        assert_eq!(result_pptx.unwrap(), APPLICATION_PPTX);
+
+        // Testing for txt detection
+        let file_path_txt = "tests/inputs/test_txt_1.txt";
+        let result_txt = determine_mime_type(file_path_txt);
+
+        assert!(result_txt.is_some());
+        assert_eq!(result_txt.unwrap().type_(), TEXT);
+
+        // Testing for csv detection
+        let file_path_csv = "tests/inputs/test_csv_1.csv";
+        let result_csv = determine_mime_type(file_path_csv);
+
+        assert!(result_csv.is_some());
+        assert_eq!(result_csv.unwrap().type_(), TEXT);
+
+        // Testing for json detection
+        let file_path_json = "tests/inputs/test_json_1.json";
+        let result_json = determine_mime_type(file_path_json);
+
+        assert!(result_json.is_some());
+        assert_eq!(result_json.unwrap().type_(), TEXT);
+
+        // Testing for png detection
+        let file_path_png = "tests/inputs/test_png_1.png";
+        let result_png = determine_mime_type(file_path_png);
+
+        assert!(result_png.is_some());
+        assert_eq!(result_png.unwrap().type_(), IMAGE);
+
+        // Testing for jpg detection
+        let file_path_jpg = "tests/inputs/test_jpg_1.jpg";
+        let result_jpg = determine_mime_type(file_path_jpg);
+
+        assert!(result_jpg.is_some());
+        assert_eq!(result_jpg.unwrap().type_(), IMAGE);
+
+        // Testing for webp detection
+        let file_path_webp = "tests/inputs/test_webp_1.webp";
+        let result_webp = determine_mime_type(file_path_webp);
+
+        assert!(result_webp.is_some());
+        assert_eq!(result_webp.unwrap().type_(), IMAGE);
+    }
+}
+
+// TOFIX: Make path sourcing platform agnostic
--- parser-web/src/routes/parse.rs
+++ parser-web/src/routes/parse.rs
@@ -1,23 +1,11 @@
 use crate::errors::ApiError;
 use actix_multipart::Multipart;
 use actix_web::{post, HttpResponse};
-use parser_core::parsers::{
-    parse_docx, parse_image, parse_pdf, parse_pptx, parse_text, parse_xlsx,
-};
-
 use futures_util::TryStreamExt;
-use infer;
-use mime::{Mime, APPLICATION_PDF, IMAGE, TEXT, TEXT_PLAIN};
+use parser_core::parsers::parse_any;
 use serde::Serialize;
-use std::{fs::read_to_string, io::Write};
+use std::io::Write;
 use tempfile::NamedTempFile;
-
-// Ttypes not defined in the mime package
-const APPLICATION_DOCX: &str =
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-const APPLICATION_XLSX: &str = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-const APPLICATION_PPTX: &str =
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation";
 
 #[derive(Serialize)]
 struct Response {
@@ -45,24 +33,10 @@ struct Response {
 async fn parse_file(mut payload: Multipart) -> Result<HttpResponse, ApiError> {
     let temp_file = create_temp_file(&mut payload).await?;
     let temp_file_path = get_temp_file_path(&temp_file)?;
-    let content_type = determine_mime_type(temp_file_path);
 
     // Need to find better way to map errors
-    let parsed_text = match content_type.as_ref() {
-        Some(mime) if *mime == APPLICATION_PDF => parse_pdf(temp_file_path)?,
-        Some(mime) if *mime == APPLICATION_DOCX => parse_docx(temp_file_path)?,
-        Some(mime) if *mime == APPLICATION_XLSX => parse_xlsx(temp_file_path)?,
-        Some(mime) if *mime == APPLICATION_PPTX => parse_pptx(temp_file_path)?,
-        Some(mime) if mime.type_() == TEXT => parse_text(temp_file_path)?,
-        Some(mime) if mime.type_() == IMAGE => parse_image(temp_file_path)?,
-        Some(mime) => {
-            return Err(ApiError::BadRequest(format!(
-                "Unsupported mime type: {}",
-                mime
-            )))
-        }
-        None => return Err(ApiError::BadRequest("Missing content type".to_string())),
-    };
+    let parsed_text =
+        parse_any(temp_file_path).map_err(|e| ApiError::InternalError(e.to_string()))?;
 
     Ok(HttpResponse::Ok().json(Response { text: parsed_text }))
 }
@@ -97,21 +71,6 @@ fn get_temp_file_path(temp_file: &NamedTempFile) -> Result<&str, ApiError> {
         .ok_or_else(|| ApiError::InternalError("Invalid temporary file path".to_string()))
 }
 
-fn determine_mime_type(file_path: &str) -> Option<Mime> {
-    // First try to detect using file signatures
-    if let Some(kind) = infer::get_from_path(file_path).ok().flatten() {
-        if let Ok(mime) = kind.mime_type().parse() {
-            return Some(mime);
-        }
-    }
-
-    // TODO: Add specific function for special text data that needs formatting like CSV etc..
-    // TODO: Maybe add checks for false positive images, like svg that may be coerced to text but shouldnt.
-
-    // If no specific type was detected, check if it's readable as text
-    read_to_string(file_path).ok().map(|_| TEXT_PLAIN)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -129,79 +88,4 @@ mod tests {
 
         assert!(result.len() > 0);
     }
-
-    #[test]
-    fn determine_mime_success() {
-        // Testing for pdf detection
-        let file_path_pdf = "tests/inputs/test_pdf_1.pdf";
-        let result_pdf = determine_mime_type(file_path_pdf);
-
-        assert!(result_pdf.is_some());
-        assert_eq!(result_pdf.unwrap(), APPLICATION_PDF);
-
-        // Testing for docx detection
-        let file_path_docx = "tests/inputs/test_docx_1.docx";
-        let result_docx = determine_mime_type(file_path_docx);
-
-        assert!(result_docx.is_some());
-        assert_eq!(result_docx.unwrap(), APPLICATION_DOCX);
-
-        // Testing for xlsx detection
-        let file_path_xlsx = "tests/inputs/test_xlsx_1.xlsx";
-        let result_xlsx = determine_mime_type(file_path_xlsx);
-
-        assert!(result_xlsx.is_some());
-        assert_eq!(result_xlsx.unwrap(), APPLICATION_XLSX);
-
-        // Testing for pptx detection
-        let file_path_pptx = "tests/inputs/test_pptx_1.pptx";
-        let result_pptx = determine_mime_type(file_path_pptx);
-
-        assert!(result_pptx.is_some());
-        assert_eq!(result_pptx.unwrap(), APPLICATION_PPTX);
-
-        // Testing for txt detection
-        let file_path_txt = "tests/inputs/test_txt_1.txt";
-        let result_txt = determine_mime_type(file_path_txt);
-
-        assert!(result_txt.is_some());
-        assert_eq!(result_txt.unwrap().type_(), TEXT);
-
-        // Testing for csv detection
-        let file_path_csv = "tests/inputs/test_csv_1.csv";
-        let result_csv = determine_mime_type(file_path_csv);
-
-        assert!(result_csv.is_some());
-        assert_eq!(result_csv.unwrap().type_(), TEXT);
-
-        // Testing for json detection
-        let file_path_json = "tests/inputs/test_json_1.json";
-        let result_json = determine_mime_type(file_path_json);
-
-        assert!(result_json.is_some());
-        assert_eq!(result_json.unwrap().type_(), TEXT);
-
-        // Testing for png detection
-        let file_path_png = "tests/inputs/test_png_1.png";
-        let result_png = determine_mime_type(file_path_png);
-
-        assert!(result_png.is_some());
-        assert_eq!(result_png.unwrap().type_(), IMAGE);
-
-        // Testing for jpg detection
-        let file_path_jpg = "tests/inputs/test_jpg_1.jpg";
-        let result_jpg = determine_mime_type(file_path_jpg);
-
-        assert!(result_jpg.is_some());
-        assert_eq!(result_jpg.unwrap().type_(), IMAGE);
-
-        // Testing for webp detection
-        let file_path_webp = "tests/inputs/test_webp_1.webp";
-        let result_webp = determine_mime_type(file_path_webp);
-
-        assert!(result_webp.is_some());
-        assert_eq!(result_webp.unwrap().type_(), IMAGE);
-    }
 }
-
-// TOFIX: Make path sourcing platform agnostic
