31aa92b9cb1e67d0ef2d406e473b35a86b59304a
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-03-20 12:09:46 -0400

    used an iterator

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use parser_core::{parse, ParserError};
 use rayon::prelude::*;
-use std::{fs::read, path::PathBuf};
+use std::{fs::read, io::Error, path::PathBuf};
 
 /// CLI arguments parser
 #[derive(Parser)]
@@ -14,12 +14,11 @@ pub struct Cli {
 
 /// Parses files in parallel and returns a Result containing either all parsed texts or the first error
 pub fn parse_files(paths: &[PathBuf]) -> Result<Vec<String>, ParserError> {
-    let mut files = Vec::new();
-
-    // Read file data
-    for path in paths {
-        files.push(read(path)?);
-    }
+    // Read files into memory
+    let files = paths
+        .iter()
+        .map(|path| read(path))
+        .collect::<Result<Vec<Vec<u8>>, Error>>()?;
 
     // Process files in parallel
     files
